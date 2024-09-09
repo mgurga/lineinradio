1364879da3ee268fd1c256a03e3ff7c1fa3ea3ed
mgurga <motiurga@gmail.com>
2024-09-09 09:04:59 -0700

    create schedule if night_duties was missed

--- scheduler/views.py
+++ scheduler/views.py
@@ -3,10 +3,14 @@ from datetime import datetime
 from django.shortcuts import render
 
 from .models import Schedule
+from .tasks import download_bloc, generate_schedule
 
 
 def schedule_page(request):
     if Schedule.objects.filter(date=datetime.now().date()).exists():
         schedule = Schedule.objects.filter(date=datetime.now().date())[0]
+    else:
+        schedule = generate_schedule()
+        download_bloc()
 
     return render(request, "schedule.html", {"sch": schedule})
