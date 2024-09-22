4a9dff2258180cdf7249de1065c0882b5aa9e9be
mgurga <motiurga@gmail.com>
2024-09-22 16:34:08 -0700

    added sample mpd config file

--- lineinradio/settings.py
+++ lineinradio/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-j8fq)zwjz5*&=#rk05t8f2*l6)=9iq-r=mx!vkyb^!g^g)jl))"
+MPD_PASSWORD = "mpdpassword"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
